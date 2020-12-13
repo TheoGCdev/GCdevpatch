e4f4389c85c038c539c4a05d268179659081143c
TheoGCdev <75916816+TheoGCdev@users.noreply.github.com>
2020-12-13 01:22:30 +0100

    Add files via upload

---
+++ patch.sh
@@ -0,0 +1,10 @@
+#[core]
+#	repositoryformatversion = 0
+#	filemode = true
+#	bare = false
+#	logallrefupdates = true
+#[remote "origin"]
+#	url = https://github.com/wazo-platform/wazo-ansible.git
+#	fetch = +refs/heads/*:refs/remotes/origin/*
+	
+echo "Patch realisé avec succès par Théo"
